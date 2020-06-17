b6589666fda8113545bcb199af4934d643b2c770
Ilya Babkov <luminozen@gmail.com>
2020-06-17 04:22:46 +0300

    Refactored DataStreamSerializer

--- src/com/topjava/basejava/webapp/common/MainConcurrency.java
+++ src/com/topjava/basejava/webapp/common/MainConcurrency.java
@@ -1,11 +1,34 @@
 package com.topjava.basejava.webapp.common;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class MainConcurrency {
     private static final Object lock1 = new Object();
     private static final Object lock2 = new Object();
+    private int counter = 0;
 
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
+    public static void main(String[] args) throws InterruptedException {
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(10000);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for(int i =0; i < 10000; i++) {
+        executorService.submit(() ->
+                {
+                    for (int j = 0; j < 100; j++) {
+                        mainConcurrency.inc();
+                    }
+                    latch.countDown();
+                });
+        }
+        latch.await(20, TimeUnit.SECONDS);
+        executorService.shutdown();
+        System.out.println(mainConcurrency.counter);
+
+        /*Thread thread1 = new Thread(() -> {
             deadLock(lock1, lock2, 1);
         });
 
@@ -18,20 +41,24 @@ public class MainConcurrency {
 
 
         thread1.start();
-        thread2.start();
+        thread2.start();*/
     }
 
     public static void deadLock(Object lock1, Object lock2, int id) {
         synchronized (lock1) {
-                System.out.println(id+": 1");
+            System.out.println(id + ": 1");
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             synchronized (lock2) {
-                    System.out.println(id+": 2");
-                }
+                System.out.println(id + ": 2");
+            }
         }
+    }
+
+    public synchronized void inc() {
+        counter++;
     }
 }
--- src/com/topjava/basejava/webapp/storage/serialization/DataStreamSerializer.java
+++ src/com/topjava/basejava/webapp/storage/serialization/DataStreamSerializer.java
@@ -1,10 +1,9 @@
 package com.topjava.basejava.webapp.storage.serialization;
-
 import com.topjava.basejava.webapp.model.*;
-
 import java.io.*;
 import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -17,14 +16,13 @@ public class DataStreamSerializer implements StreamSerializer {
 
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactsType, String> entry : contacts.entrySet()) {
+
+            writeCollection(dos, contacts.entrySet(), entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
+            });
 
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
+            writeCollection(dos, sections.entrySet(), entry -> {
                 SectionType type = entry.getKey();
                 switch (type) {
                     case PERSONAL:
@@ -34,22 +32,24 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        List<String> items = ((ListSection) entry.getValue()).getContent();
                         dos.writeUTF(entry.getKey().name());
-                        dos.writeInt(items.size());
-                        for (String item : items) {
-                            dos.writeUTF(item);
-                        }
+                        writeCollection(dos, ((ListSection) entry.getValue()).getContent(), dos::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organization> organizations = ((OrganisationSection) entry.getValue()).getOrganizations();
                         dos.writeUTF(entry.getKey().name());
-                        dos.writeInt(organizations.size());
-                        writeOrganizations(dos, organizations);
-                        break;
+                        writeCollection(dos, ((OrganisationSection) entry.getValue()).getOrganizations(), item -> {
+                            dos.writeUTF(item.getHomePage().getTitle());
+                            dos.writeUTF(item.getHomePage().getUrl());
+                            writeCollection(dos, item.getPositions(), position -> {
+                                dos.writeUTF(position.getPosition());
+                                dos.writeUTF(position.getStartDate().toString());
+                                dos.writeUTF(position.getEndDate().toString());
+                                dos.writeUTF(position.getDescription());
+                            });
+                        });
                 }
-            }
+            });
         }
     }
 
@@ -57,18 +57,11 @@ public class DataStreamSerializer implements StreamSerializer {
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
             Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-            int size = dis.readInt();
-            SectionType type;
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactsType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-
-            size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                type = SectionType.valueOf(dis.readUTF());
+            readItems(dis, () -> resume.addContact(ContactsType.valueOf(dis.readUTF()), dis.readUTF()));
+            readItems(dis, () -> {
+                SectionType type = SectionType.valueOf(dis.readUTF());
                 resume.addSection(type, readSection(dis, type));
-            }
-
+            });
             return resume;
         }
     }
@@ -80,54 +73,50 @@ public class DataStreamSerializer implements StreamSerializer {
                 return new TextSection(dis.readUTF());
             case QUALIFICATIONS:
             case ACHIEVEMENT:
-                return new ListSection(readList(dis, dis.readInt()));
+                return new ListSection(readList(dis, dis::readUTF));
             case EXPERIENCE:
             case EDUCATION:
-                return new OrganisationSection(readOrganizations(dis, dis.readInt()));
+                return new OrganisationSection(readList(dis, ()->new Organization(
+                        new Link(dis.readUTF(), dis.readUTF()), readList(dis, ()->new Organization.Position(
+                                dis.readUTF(), YearMonth.parse(dis.readUTF()), YearMonth.parse(dis.readUTF()), dis.readUTF()
+                        )))
+                ));
         }
         return null;
     }
 
-    public List<String> readList(DataInputStream dis, int size) throws IOException {
-        List<String> list = new ArrayList<>();
+    private interface ElementWriter<T> {
+        void write(T t) throws IOException;
+    }
+
+    private interface ElementProcessor {
+        void process() throws IOException;
+    }
+
+    private interface ElementReader<T> {
+        T read() throws IOException;
+    }
+
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, ElementWriter<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            writer.write(item);
+        }
+    }
+
+    private void readItems(DataInputStream dis, ElementProcessor processor) throws IOException {
+        int size = dis.readInt();
         for (int i = 0; i < size; i++) {
-            list.add(dis.readUTF());
+            processor.process();
+        }
+    }
+
+    private <T> List<T> readList(DataInputStream dis, ElementReader<T> reader) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < size; i++){
+            list.add(reader.read());
         }
         return list;
-    }
-
-    public void writeOrganizations(DataOutputStream dos, List<Organization> list) throws IOException {
-        List<Organization.Position> positions;
-        Link homePage;
-        for (Organization item : list) {
-            positions = item.getPositions();
-            homePage = item.getHomePage();
-            dos.writeUTF(homePage.getTitle());
-            dos.writeUTF(homePage.getUrl());
-            dos.writeInt(positions.size());
-            for(Organization.Position position: positions) {
-                dos.writeUTF(position.getPosition());
-                dos.writeUTF(position.getStartDate().toString());
-                dos.writeUTF(position.getEndDate().toString());
-                dos.writeUTF(position.getDescription());
-            }
-        }
-    }
-
-    public List<Organization> readOrganizations(DataInputStream dis, int size) throws IOException {
-        List<Organization> organizations = new ArrayList<>();
-        for(int i = 0; i < size; i++) {
-            Link homePage = new Link(dis.readUTF(), dis.readUTF());
-            organizations.add(new Organization(homePage, readPositions(dis, dis.readInt())));
-        }
-        return organizations;
-    }
-
-    public List<Organization.Position> readPositions(DataInputStream dis, int size) throws IOException {
-        List<Organization.Position> positions = new ArrayList<>();
-        for(int i = 0; i < size; i++) {
-            positions.add(new Organization.Position(dis.readUTF(), YearMonth.parse(dis.readUTF()), YearMonth.parse(dis.readUTF()), dis.readUTF()));
-        }
-        return  positions;
     }
 }
